9dab59134795d334458bd33862422673a2dd3b4c
David Zhu <dazhua@l4712-13.info.polymtl.ca>
2023-09-24 10:15:07 -0400

    tbb successful test

--- labo-1/source/pipeline-tbb.cpp
+++ labo-1/source/pipeline-tbb.cpp
@@ -5,6 +5,41 @@ extern "C" {
 #include "pipeline.h"
 }
 
+#include <iostream>
+#include "tbb/pipeline.h"
+
+float* GenerateCoucou(tbb::flow_control& fc) {
+    while (true) {
+        std::cout << "coucou" << std::endl;
+        // Ajoutez un délai si nécessaire pour contrôler la fréquence d'affichage
+        // tbb::this_tbb_thread::sleep(tbb::tick_count::interval_t(1));
+        // if (fc.is_cancelled()) {
+        //     return nullptr;
+        // }
+    }
+}
+
 int pipeline_tbb(image_dir_t* image_dir) {
+    std::cout << "ok";
+
+    parallel_pipeline(16, make_filter<void, image_t*>(tbb::filter::serial, [&](tbb::flow_control& fc) -> image_t* {
+                              std::cout << "\n debut";
+                              image_t* image;
+                              image = image_dir_load_next((image_dir_t*)image_dir);
+                              if (image == NULL) {
+                                  fc.stop();
+                              }
+                              std::cout << ".";
+                              return image;
+                          }) 
+                        //   & make_filter<image_t*, image_t*>(tbb::filter::serial, [&](image_t* image) {
+                        //       std::cout << "deuxieme";
+                        //       return 0;
+                        //   })
+                          & make_filter<image_t*, void>(tbb::filter::serial, [&](image_t* image) {
+                              std::cout << "troisieme";
+                              return 0;
+                          })
+                          );
     return -1;
 }
