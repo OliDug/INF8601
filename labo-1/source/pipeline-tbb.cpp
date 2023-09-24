9bc448d1b88839e3ccc3b587808210813690be72
Olivier Duguay <oldug@l4712-12.info.polymtl.ca>
2023-09-24 18:00:22 -0400

    Dernières modif, tbb est plus vite que pthread

--- labo-1/source/pipeline-tbb.cpp
+++ labo-1/source/pipeline-tbb.cpp
@@ -11,46 +11,44 @@ extern "C" {
 int pipeline_tbb(image_dir_t* image_dir) {
     std::cout << "TBB";
 
-    parallel_pipeline(48, make_filter<void, image_t*>(tbb::filter::serial,
-                                                      [&](tbb::flow_control& fc) -> image_t* {
-                                                          image_t* image;
-                                                          image = image_dir_load_next((image_dir_t*)image_dir);
-                                                          if (image == NULL) {
-                                                              fc.stop();
-                                                          }
-                                                          return image;
-                                                      })
+    parallel_pipeline(48, 
+        make_filter<void, image_t*>(tbb::filter::serial, [&](tbb::flow_control& fc) -> image_t* {
+            image_t* image;
+            image = image_dir_load_next((image_dir_t*)image_dir);
+            if (image == NULL) {
+                fc.stop();
+            }
+            return image;
+        })
 
-                              & make_filter<image_t*, image_t*>(tbb::filter::parallel,
-                                                                [](image_t* image) {
-                                                                    // std::cout << "_scale_up";
-                                                                    image_t* image2 = filter_scale_up(image, 2);
-                                                                    image_destroy(image);
-                                                                    return image2;
-                                                                })
+        & make_filter<image_t*, image_t*>(tbb::filter::parallel, [](image_t* image) {
+            // std::cout << "_scale_up";
+            image_t* image2 = filter_scale_up(image, 2);
+            image_destroy(image);
+            return image2;
+        })
 
-                              & make_filter<image_t*, image_t*>(tbb::filter::parallel,
-                                                                [](image_t* image) {
-                                                                    // std::cout << "_sharpen";
-                                                                    image_t* image3 = filter_sharpen(image);
-                                                                    image_destroy(image);
-                                                                    return image3;
-                                                                })
+        & make_filter<image_t*, image_t*>(tbb::filter::parallel, [](image_t* image) {
+            // std::cout << "_sharpen";
+            image_t* image3 = filter_sharpen(image);
+            image_destroy(image);
+            return image3;
+        })
 
-                              & make_filter<image_t*, image_t*>(tbb::filter::parallel,
-                                                                [](image_t* image) {
-                                                                    // std::cout << "_sobel";
-                                                                    image_t* image4 = filter_sobel(image);
-                                                                    image_destroy(image);
-                                                                    return image4;
-                                                                })
+        & make_filter<image_t*, image_t*>(tbb::filter::parallel, [](image_t* image) {
+            // std::cout << "_sobel";
+            image_t* image4 = filter_sobel(image);
+            image_destroy(image);
+            return image4;
+        })
 
-                              & make_filter<image_t*, void>(tbb::filter::parallel, [&](image_t* image) {
-                                    image_dir_save(image_dir, image);
-                                    // std::cout << "_save";
-                                    std::cout << ".";
-                                    image_destroy(image);
-                                    return 0;
-                                }));
+        & make_filter<image_t*, void>(tbb::filter::parallel, [&](image_t* image) {
+            image_dir_save(image_dir, image);
+            // std::cout << "_save";
+            std::cout << ".";
+            image_destroy(image);
+            return 0;
+        })
+    );
     return -1;
 }
