9a43c2b69ed044837fd373f9ecab42b7d1692981
Olivier Duguay <olivier.duguay@polymtl.ca>
2023-09-28 23:50:55 -0400

    Débuggage de pthread, le signal qu'un type de thread est terminé est maintenant envoyé de pipeline_thread, cause un timeout, mais plus loin dans handout submit

--- labo-1/source/pipeline-pthread.c
+++ labo-1/source/pipeline-pthread.c
@@ -10,17 +10,21 @@ queue_t* scale_up_queue;
 queue_t* sharpen_queue;
 queue_t* sobel_queue;
 
-bool load_done;
-bool scale_up_done;
-bool sharpen_done;
-bool sobel_done;
+bool load_done = false;
+bool scale_up_done = false;
+bool sharpen_done = false;
+bool sobel_done = false;
+
+pthread_mutex_t mutex_load;
+pthread_mutex_t mutex_scale_up;
+pthread_mutex_t mutex_sharpen;
+pthread_mutex_t mutex_sobel;
 
 void* thread_load(void* image_dir) {
     image_t* image;
     while (1) {
         image = image_dir_load_next((image_dir_t*)image_dir);
         if (image == NULL) {
-            load_done = true;
             break;
         }
 		queue_push(load_queue, image);
@@ -32,13 +36,16 @@ void* thread_load(void* image_dir) {
 void *thread_scale_up(void *inutilise) {
 	image_t* image1;
 	size_t queue_used;
+	bool canStop = false;
 	while(1) {
 		pthread_mutex_lock(&load_queue->mutex);
 		queue_used = load_queue->used;
 		pthread_mutex_unlock(&load_queue->mutex);
 		if (queue_used == 0) {
-			if (load_done == true) {
-				scale_up_done = true;
+			pthread_mutex_lock(&mutex_load);
+			canStop = load_done;
+			pthread_mutex_unlock(&mutex_load);
+			if (canStop == true) {
 				break;
 			}
 			continue;
@@ -56,13 +63,16 @@ void *thread_sharpen(void *inutilise) {
 	image_t* image2;
 	image_t* image3;
 	size_t queue_used;
+	bool canStop = false;
 	while(1) {
 		pthread_mutex_lock(&scale_up_queue->mutex);
 		queue_used = scale_up_queue->used;
 		pthread_mutex_unlock(&scale_up_queue->mutex);
 		if (queue_used == 0) {
-			if (scale_up_done == true) {
-				sharpen_done = true;
+			pthread_mutex_lock(&mutex_scale_up);
+			canStop = scale_up_done;
+			pthread_mutex_unlock(&mutex_scale_up);
+			if (canStop == true) {
 				break;
 			}
 			continue;
@@ -80,13 +90,16 @@ void *thread_sobel(void *inutilise) {
 	image_t* image3;
 	image_t* image4;
 	size_t queue_used;
+	bool canStop = false;
 	while(1) {
 		pthread_mutex_lock(&sharpen_queue->mutex);
 		queue_used = sharpen_queue->used;
 		pthread_mutex_unlock(&sharpen_queue->mutex);
 		if (queue_used == 0) {
-			if (sharpen_done == true) {
-				sobel_done = true;
+			pthread_mutex_lock(&mutex_sharpen);
+			canStop = sharpen_done;
+			pthread_mutex_unlock(&mutex_sharpen);
+			if (canStop == true) {
 				break;
 			}
 			continue;
@@ -103,13 +116,18 @@ void *thread_sobel(void *inutilise) {
 void *thread_save(void *image_dir) {
 	image_t* image4;
 	size_t queue_used;
+	bool canStop = false;
 	while(1) {
 		pthread_mutex_lock(&sobel_queue->mutex);
 		queue_used = sobel_queue->used;
 		pthread_mutex_unlock(&sobel_queue->mutex);
 		if (queue_used == 0) {
-			if (sobel_done == true)
+			pthread_mutex_lock(&mutex_sobel);
+			canStop = sobel_done;
+			pthread_mutex_unlock(&mutex_sobel);
+			if (canStop == true) {
 				break;
+			}
 			continue;
 		}
 		image4 = queue_pop(sobel_queue);
@@ -123,18 +141,24 @@ void *thread_save(void *image_dir) {
 }
 
 int pipeline_pthread(image_dir_t* image_dir) {
-    int i             = 0;
-    int num_thLoad    = 2;
-    int num_thScaleUp = 6;
-    int num_thSharpen = 12;
-    int num_thSobel   = 16;
-    int num_thSave    = 12;
+    int i = 0;
+
+	const int num_thLoad    = 2;
+	const int num_thScaleUp = 6;
+	const int num_thSharpen = 15;
+	const int num_thSobel   = 16;
+	const int num_thSave    = 12;
 
     pthread_t thLoad[num_thLoad];
     pthread_t thScaleUp[num_thScaleUp];
     pthread_t thSharpen[num_thSharpen];
     pthread_t thSobel[num_thSobel];
     pthread_t thSave[num_thSave];
+
+	pthread_mutex_init(&mutex_load, NULL);
+	pthread_mutex_init(&mutex_scale_up, NULL);
+	pthread_mutex_init(&mutex_sharpen, NULL);
+	pthread_mutex_init(&mutex_sobel, NULL);
 
     load_queue     = queue_create(69120000);  // 100 images max (691200 bytes par image)
     scale_up_queue = queue_create(6912000);
@@ -160,15 +184,31 @@ int pipeline_pthread(image_dir_t* image_dir) {
     for (i = 0; i < num_thLoad; i++) {
         pthread_join(thLoad[i], NULL);
     }
+	pthread_mutex_lock(&mutex_load);
+	load_done = true;
+	pthread_mutex_unlock(&mutex_load);
+
     for (i = 0; i < num_thScaleUp; i++) {
         pthread_join(thScaleUp[i], NULL);
     }
+	pthread_mutex_lock(&mutex_scale_up);
+	scale_up_done = true;
+	pthread_mutex_unlock(&mutex_scale_up);
+
     for (i = 0; i < num_thSharpen; i++) {
         pthread_join(thSharpen[i], NULL);
     }
+	pthread_mutex_lock(&mutex_sharpen);
+	sharpen_done = true;
+	pthread_mutex_unlock(&mutex_sharpen);
+
     for (i = 0; i < num_thSobel; i++) {
         pthread_join(thSobel[i], NULL);
     }
+	pthread_mutex_lock(&mutex_sobel);
+	sobel_done = true;
+	pthread_mutex_unlock(&mutex_sobel);
+
     for (i = 0; i < num_thSave; i++) {
         pthread_join(thSave[i], NULL);
     }
